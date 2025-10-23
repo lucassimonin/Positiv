46cbcfd51ebb64f5fd04dd0193b696f443d615dc
DnD-Luk <luk@dnd.fr>
2025-10-23 22:03:23 +0200

    Initial Commit

---
+++ Positiv/PositivApp.swift
@@ -0,0 +1,17 @@
+//
+//  PositivApp.swift
+//  Positiv
+//
+//  Created by DnD-Luk on 23/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct PositivApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
